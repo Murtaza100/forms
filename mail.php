fc54cc1756047f499238d0d0a0d018405c01ec6f
Murtaza <65944085+Murtaza100@users.noreply.github.com>
2021-06-06 22:35:36 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -6,7 +6,7 @@ $message= $_POST['message'];
 $to = "najmimurtaza2@mail.com";
 $subject = "Mail From needygeeks";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@needygeeks.com" . "\r\n" .
+$headers = "From: https://murtaza100.github.io/forms/" . "\r\n" .
 "CC: shamsspd03@gmail.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
