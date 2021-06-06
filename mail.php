fab2a7e99d2b8eba0a6362fe653377ec7317055b
Murtaza <65944085+Murtaza100@users.noreply.github.com>
2021-06-06 22:36:41 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -6,8 +6,8 @@ $message= $_POST['message'];
 $to = "najmimurtaza2@mail.com";
 $subject = "Mail From needygeeks";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: https://murtaza100.github.io/forms/" . "\r\n" .
-"CC: shamsspd03@gmail.com";
+$headers = "From: " shamsspd03@gmail.com "\r\n" .
+"CC: ;
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
